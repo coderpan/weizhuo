4be0e3481b9b57580b8f4686be55c969b255794f
philpan <804570192@qq.com>
2017-01-16 00:23:39 +0800

    文件上传使用multer模块

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,5 @@
 /**
- * @api {post} /api/shop/upload 文件上传
+ * @api {post} /api/upload 文件上传
  * @apiName upload
  * @apiGroup  Shop
  * @apiVersion 0.1.0
@@ -26,10 +26,63 @@
  * @apiError (错误码) 101 未知错误
  * @apiError (错误码) 1050  文件上传失败
  * @apiError (错误码) 1051  文件大小不能超过2M
+ * @apiError (错误码) 1052  文件类型错误，仅支持JPG
  */
 
-router.post('/upload', function(req, res, next) {
-    
-});
+var multerutil = require('./multerUtil');
+var multerupload=multerutil.single('thumbnail');            
+    exports.dataInput = function (req, res) {
+        multerupload(req, res, function (err) {
+            //添加错误处理
+            if (err) {
+                result = {
+                    code: 1050,
+                    msg:'上传文件失败'
+                };
+                return res.json(result);
+            } 
+            //文件信息在req.file或者req.files中显示。
+            //console.log(req);
+            result = {
+                code: 0,
+                url:'https://www.wxpuu.com/files/'+req.file.filename
+            };
+            return res.json(result);
+        });
+ }
 
-};
+/*
+router.post('/', function(req, res, next) {
+    if (req.files.thumbnail.size > 2 * 1024 * 1024) {
+        result = {
+            code: 1051,
+            msg:'文件大小不能超过2M'
+        };
+        return res.json(result);
+    }
+    if (req.files.thumbnail.type != "jpg") {
+        result = {
+            code: 1052,
+            msg:'文件类型错误，仅支持JPG'
+        };
+        return res.json(result);
+    }
+    // 获得文件的临时路径
+    var tmp_path = req.files.thumbnail.path;
+    // 指定文件上传后的目录
+    var target_path = '/data/wxpuu/images/' + req.files.thumbnail.name;
+    // 移动文件
+    fs.rename(tmp_path, target_path, function(err) {
+      if (err) {
+        result = {
+            code: 1050,
+            msg:'上传文件失败'
+        };
+      }
+      // 删除临时文件夹文件, 
+      fs.unlink(tmp_path, function() {
+         if (err) throw err;
+      });
+    });
+});
+*/
